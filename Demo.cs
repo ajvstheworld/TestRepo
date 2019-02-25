dc25bf900335d6e185ec437ea78d27e66a6afb7e
ajvstheworld <austinjohnson2345@gmail.com>
2019-02-25 10:04:32 -0600

    the beginning

---
+++ Demo.cs
@@ -0,0 +1,15 @@
+using System;
+namespace Demo
+{
+    // A simple application using C#
+    class Program
+    {
+        // The main function
+        static void Main(string[] args)
+        {
+            // Writing hello world to the console
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
